82fbd537f6b1cac61113cef68d6d528182145593
duy <duyme18@gmail.com>
2019-10-23 10:41:17 +0700

    add project

---
+++ src/app/progress-bar/progress-bar.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.css']
+})
+export class ProgressBarComponent implements OnInit {
+
+  @Input() backgroundColor = '#1k1k1k';
+  @Input() progressColor = '#4CAF20';
+  @Input() width = 50;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
